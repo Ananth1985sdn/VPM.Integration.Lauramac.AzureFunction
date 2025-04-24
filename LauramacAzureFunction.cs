76244c54389ffa4d4a213b9ef59d78022e48beb6
Ananth Ganapathy <ananth.ganapathy@mgtechsoft.com>
2025-04-23 22:35:15 -0700

    intead of break added continue

--- LauramacAzureFunction.cs
+++ LauramacAzureFunction.cs
@@ -121,7 +121,7 @@ namespace VPM.Integration.Lauramac.AzureFunction
                         catch (Newtonsoft.Json.JsonReaderException ex)
                         {
                             _logger.LogInformation("Invalid JSON in Loan Documents Response: {Response}. Error: {ErrorMessage}",documentsResponse, ex.Message);
-                            return;
+                            continue;
                         }
 
                         _logger.LogInformation($"Attachments for Loan {loan.Fields.LoanNumber}: {documentsResponse}");
